143c637342a7ee684b9d3229340e38bf7774bbb0
ChchPtatoFvr <devinscholl1@gmail.com>
2019-11-27 11:56:00 -0800

    Setup webpack and babel

---
+++ webpack.dev.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+const resolve = relative_path => path.resolve(__dirname, relative_path);
+
+
+module.exports = {
+  entry: resolve('src/main.js'),
+  target: 'web',
+  resolve: {
+    alias: {
+      '@src': resolve('src')
+    }
+  },
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: resolve('docs'),
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          babelrc: true
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: resolve('docs/index.html'),
+      template: resolve('src/template.html')
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
---
+++ webpack.prod.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+const resolve = relative_path => path.resolve(__dirname, relative_path);
+
+
+module.exports = {
+  entry: resolve('./src/main.js'),
+  target: 'web',
+  resolve: {
+    alias: {
+      '@src': resolve('./src')
+    }
+  },
+  output: {
+    path: resolve('./docs'),
+    publicPath: '/'
+  },
+  mode: 'production',
+  devtool: 'nosources-source-map',
+  output: {
+    filename: '[hash].bundle.js',
+    chunkFilename: '[chunkhash].chunk.js',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      minSize: 0,
+      maxInitialRequests: Infinity,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: (module) => {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName}`;
+          },
+          chunks: 'all',
+          maxSize: 244000
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          babelrc: true
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: resolve('./docs/index.html'),
+      template: resolve('./src/template.html')
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
